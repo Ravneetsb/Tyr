c6688ccf1df9908fbe2385d7929d601cb9732f56
Ravneet Singh Bhatia <ravneet.bhatia@outlook.com>
2024-10-10 14:20:01 -0700

    creating crimeService

---
+++ src/main/java/org/rsb/tyr/services/CrimeService.java
@@ -0,0 +1,30 @@
+package org.rsb.tyr.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.rsb.tyr.enums.CrimeType;
+import org.rsb.tyr.models.Crime;
+import org.rsb.tyr.repositories.CrimeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrimeService {
+
+  private final CrimeRepository crimeRepository;
+
+  @Autowired
+  public CrimeService(CrimeRepository crimeRepository) {
+    this.crimeRepository = crimeRepository;
+  }
+
+  public List<Crime> getAllCrimes() {
+    return crimeRepository.findAll();
+  }
+
+  public Crime getCrime(CrimeType type) {
+    // return crime that has the same name as the type.
+    Optional<Crime> crime = crimeRepository.findByName(type.name());
+    return crime.orElse(null);
+  }
+}
