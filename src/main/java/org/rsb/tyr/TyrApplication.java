8695e1f7acca49e31f7b0248a62d07d68f4f6732
Ravneet Singh Bhatia <ravneet.bhatia@outlook.com>
2024-10-09 22:14:06 -0700

    initial commit

---
+++ src/main/java/org/rsb/tyr/TyrApplication.java
@@ -0,0 +1,13 @@
+package org.rsb.tyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyrApplication.class, args);
+    }
+
+}
