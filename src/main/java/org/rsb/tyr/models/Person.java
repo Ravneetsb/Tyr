4b7c007e25e9da87e20c856624957e4b69eedc06
Ravneet Singh Bhatia <ravneet.bhatia@outlook.com>
2024-10-09 22:18:37 -0700

    initial commit for person

---
+++ src/main/java/org/rsb/tyr/models/Person.java
@@ -0,0 +1,10 @@
+package org.rsb.tyr.models;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node
+public class Person {
+  @Id @GeneratedValue private long id;
+}
