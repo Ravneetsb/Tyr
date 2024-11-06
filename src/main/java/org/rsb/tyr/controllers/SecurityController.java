e21a7f548c45de9d6913e6fcfbe142856fa53460
Ravneet Singh Bhatia <ravneet.bhatia@outlook.com>
2024-11-06 13:00:39 -0800

    refactor: update request mapping for SecurityController
    Change the request mapping in SecurityController from 
    "/api/check" to "/basic/check" to better reflect the 
    endpoint's purpose and improve API organization.

--- src/main/java/org/rsb/tyr/controllers/SecurityController.java
+++ src/main/java/org/rsb/tyr/controllers/SecurityController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/check")
+@RequestMapping("/basic/check")
 public class SecurityController {
   private final SecurityService securityService;
 
